8d79823ee13b42ac31fb4fee1c8cb03a0e38457d
chunghyun.lee <chunghyun.lee@kstec.co.kr>
2021-05-06 13:08:30 +0900

    4장 mustache 를 이용한 화면 구성

--- src/main/java/com/chungs/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/chungs/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.chungs.book.springboot.service.posts;
 
 import com.chungs.book.springboot.domain.posts.Posts;
 import com.chungs.book.springboot.domain.posts.PostsRepository;
+import com.chungs.book.springboot.web.dto.PostsListResponseDto;
 import com.chungs.book.springboot.web.dto.PostsResponseDto;
 import com.chungs.book.springboot.web.dto.PostsSaveRequestDto;
 import com.chungs.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +41,20 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
         );
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        postsRepository.delete(posts);
     }
 }

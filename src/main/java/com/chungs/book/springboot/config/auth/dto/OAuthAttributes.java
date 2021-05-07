34a6f10973f88d5eb766d0509ccc183c343a4c31
chunghyun lee <sinna94@naver.com>
2021-05-07 13:24:25 +0900

    6장 스프링 시큐리티와 OAuth 2.0 으로 로그인

--- src/main/java/com/chungs/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/chungs/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -31,7 +31,23 @@ public class OAuthAttributes {
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
+
+        if ("naver".equals(registrationId)) {
+            return ofNaver(attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey("id")
+                .build();
     }
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
--- src/main/java/com/chungs/book/springboot/web/IndexController.java
+++ src/main/java/com/chungs/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.chungs.book.springboot.web;
 
+import com.chungs.book.springboot.config.auth.LoginUser;
 import com.chungs.book.springboot.config.auth.dto.SessionUser;
 import com.chungs.book.springboot.service.posts.PostsService;
 import com.chungs.book.springboot.web.dto.PostsResponseDto;
@@ -16,13 +17,13 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(
+            Model model,
+            @LoginUser SessionUser user
+    ) {
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
